d82ce54f4c68c29935ae7eeea5910304b692854a
Shawn Fiske <durah_shar@hotmail.com>
2015-07-27 14:38:49 -0700

    updated user model and server db connection

--- models/user-model.js
+++ models/user-model.js
@@ -1,17 +1,9 @@
-var bcrypt      = require('bcrypt');
 var mongoose    = require('mongoose');
 var Schema      = mongoose.Schema;
 
 var userSchema  = Schema({
-  
+  name: String,
+  passwordHash: String
 });
-
-// userSchema.methods.generatHash = function(password) {
-//   returnn bcrypt.hashSync(password, bcrypt.genSaltSync(8));
-// }
-
-// userSchema.methods.checkPassword = function(password){
-//   return bcrypt.compareSync(password, this.password);
-// }
 
 module.exports  = mongoose.model('User', userSchema);
