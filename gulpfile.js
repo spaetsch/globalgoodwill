8d006c3245ad15e60b50e0cc60312a87c5a01b98
Sarah Paetsch <spaetsch@gmail.com>
2015-07-27 14:35:25 -0700

    Add Front End tasks to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
+'use strict';
+
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
+
+//Front End requires
+var sass = require('gulp-sass');
+var webpack = require('gulp-webpack');
+var minifyCss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+
 
 gulp.task('default', ['test', 'lint', 'watch'], function() {});
 
@@ -19,3 +28,53 @@ watch breaks server requests
 gulp.task('watch', function() {
   gulp.watch('*.js', ['test', 'lint']);
 });
+
+
+
+// Front End gulp task commands
+
+gulp.task('sass', function () {
+  gulp.src('./dev/Sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dev/CSS/'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./dev/Sass/**/*.scss', ['sass']);
+});
+
+gulp.task('webpackdev', function() {
+  return gulp.src('./dev/js/**.js')
+    .pipe(webpack({
+      output: {
+        filename: 'bundle.js'
+      }
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('./dev/CSS/*.css')
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./build/CSS/'));
+});
+
+gulp.task('compress', function() {
+  return gulp.src('lib/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('copy', function() {
+  var opts = {
+    conditionals: true,
+    spare:true
+  };
+    return gulp.src('dev/**/*.html')
+    .pipe(gulp.dest('build/'))
+    //.pipe(minifyHTML(opts))
+    .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('build', ['copy', 'webpackdev', 'minify-css']);
