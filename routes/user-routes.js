82b738e97f04de59f20f8eb77d50c35f4a4cb16b
Goutham Reddy <gouthamvreddy@gmail.com>
2015-07-27 16:04:50 -0700

    updated user-routes

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -3,10 +3,10 @@ var User       = require('../models/user-model.js');
 module.exports = function(router) {
 
   //list all users
-  router.get('/', function(request, response) {
+  router.get('/user', function(request, response) {
     //req.params.id
     User.find({}, function(err, data) {
-      
+
       if (err) {
         response.status(500).json({msg: 'failed'})
       }else{
