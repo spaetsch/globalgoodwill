4cf02a9a759e6e37f108e02ada5e0a024fea3654
Goutham Reddy <gouthamvreddy@gmail.com>
2015-07-28 08:53:37 -0700

    created mongoose schema for surplus, shipment, & nonprofit

---
+++ models/Surplus.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+var surplusSchema = mongoose.Schema({
+  userId: String,
+  itemName: String,
+  description: String,
+  originAddress: String,
+  originCity: String,
+  originState: String,
+  originZip: Number,
+  originCountry: String,
+  dateAvailable: Date,
+  dateExpires: Date,
+  claimed: Array
+});
+
+module.exports = mongoose.model('Surplus', surplusSchema);
