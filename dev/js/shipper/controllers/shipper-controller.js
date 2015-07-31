eaa69b0977552f6e8e3a2378c8c018108d695af5
Sarah Paetsch <spaetsch@gmail.com>
2015-07-30 23:31:11 -0700

    Shipper search seems to work...?

--- dev/js/shipper/controllers/shipper-controller.js
+++ dev/js/shipper/controllers/shipper-controller.js
@@ -1,10 +1,11 @@
 'use strict';
 
-
 module.exports = function(app) {
   app.controller('shipper-controller', ['$scope', 'resource', function($scope, resource) {
 
     var Shipper = resource('create_shipment'); //this corresponds to URL from routes
+    var searchShipper = resource('shipment');
+
     $scope.postItem = function(shipper) {
       Shipper.postItem(shipper, function(response) {
       console.log("i'm in shipper submitForm");
