1e182741dc48db3cff9310476ff0b8be43f2d5cf
Shawn Fiske <durah_shar@hotmail.com>
2015-07-30 20:01:01 -0700

    changed get to post methods

--- routes/surplus-routes.js
+++ routes/surplus-routes.js
@@ -34,11 +34,9 @@ module.exports  = function(router, passport) {
                 newSurplus.dateExpires      = req.body.dateExpires;
                 newSurplus.claimed          = req.body.claimed;
 
-                User.findOne(data.id)
+                User.find({'_id' : data.id})
                 .exec(function(err, user){
-                  newSurplus.orgName = user.organization_name;
-
-                  console.log(user);
+                  newSurplus.orgName = user[0].organization_name;
 
                   newSurplus.save(function(err, user) {
                     if (err) {
@@ -83,7 +81,7 @@ module.exports  = function(router, passport) {
   //  superagent localhost:3000/api/surplus/72YiOyFuhFTvaZhcdd27Hf7naIGBDIl8qQfRwH8tBQWp/usa/shoes get
   // '{"token":"Tm+F7CjLq0ReeOpAYvd2bx20LXg97VJpSQ1WQHSe445D","location":"usa",itemName":"shoes"}'
   router.route('/surplus/country/:location/item/:item')
-        .get(function(req, res) {
+        .post(function(req, res) {
           //req.header['token']
           console.log(req.body.token, req.params.location, req.params.item);
           decodeToken(req.body.token, function(err, data) {
